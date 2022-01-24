8f12d95ca142308cd25409077488bb6af8fac5d3
takafumi kaneko <takafumi.kaneko@rlsc.sbs-group.co.jp>
2022-01-24 20:03:24 +0900

    error_control_addition

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # wiki_related_keyword_search
 wikiの関連KW探索プログラム
+
+## 実行環境構築
+
+dokcerがinstallされている環境にてwiki_related_keyword_searchで以下のコマンドを実行
+
+```
+python main.py -t 探索するkw
+
+python main.py -t 探索するkw
+
+
+```
+
+## 実行方法
+
+wiki_related_keyword_search\srcで以下のコマンドを実行
+
+```
+python main.py -t 探索するkw
+```
+
+## 出力形式
+
+探索KW順に出力される
+ex)物流で探索した場合
+
+```
+-物流
+    -英語$
+    -ロジスティクス
+        -英語$
+        -原材料
+            -英語$
+            -製品$
+        -生産
+            -人間$
+```
+
+
+## 動作確認環境
+
+- Python 3.7
+- numpy 1.20.0
+- pandas 1.1.5
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import re
 import json
@@ -6,7 +7,6 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import argparse
 import time
-import pprint
 
 
 def get_web_page(url):
@@ -26,7 +26,7 @@ def get_web_page(url):
     """
     r = requests.get(url)  
     r.encoding = r.apparent_encoding
-    print(type(r))
+    time.sleep(1)
     return r
 
 def kw_scraping(r):
@@ -91,52 +91,92 @@ def judgement_no_target_search(kw_list, search_result):
 
     """
 
-    no_tagert_list = ['語', '学']
-    kw_list = [kw+'$' if kw[-1] in no_tagert_list else kw for kw in kw_list]
+    kw_list = [kw+'$' if kw[-1] in no_target_search_flag else kw for kw in kw_list]
 
     keys = search_result.keys()
     kw_list = [kw+'@' if kw in keys else kw for kw in kw_list]
 
     return kw_list
 
-
 def web_search(kw_list, kw_url_dict, search_result, limit_cnt):
+    """
+    対象KWのwikiの概要のリンクを探索しKWのdictを返す
+    limit_cntが上限値に達するまで再帰で探索を行う
+
+    Parameters
+    ----------
+    kw_list : list
+        探索して取得したkwのlist
+
+    kw_url_dict : dict
+        探索するkwとそのkwのリンクを格納しているdict {'物流' : 'https://ja.wikipedia.org/wiki/%E7%89%A9%E6%B5%81', ...}
+
+    search_result : dict
+        探索元のKWと取得したKWのdict {'物流': ['英語$', 'ロジスティクス', ...], 'ロジスティクス':[...], ...}
+
+    limit_cnt : int
+        探索回数のカウント
+
+    Returns
+    -------
+    search_result : dict
+        探索元のKWと取得したKWのdict {'物流': ['英語$', 'ロジスティクス', ...], 'ロジスティクス':[...], ...}
+
+    """
     next_search_kw_list = []
     next_search_url_dict = {}
 
-    for kw in kw_list:
-        if kw[-1] != '$' and kw[-1] != '@': # kwの末尾に$or@がついているkwは探索しない
-            print(kw)
-            url = kw_url_dict[kw]
-            r = get_web_page(url)
-            kw_list_tmp, kw_url_dict_tmp = kw_scraping(r)
-            kw_list_tmp = judgement_no_target_search(kw_list_tmp, search_result)
-            print(kw_list_tmp)
-            search_result[kw] = kw_list_tmp
-            next_search_kw_list += kw_list_tmp
-            next_search_url_dict = kw_url_dict_tmp | next_search_url_dict
+    if kw_list:
+        for kw in kw_list:
+            if kw[-1] != '$' and kw[-1] != '@': # kwの末尾に$or@がついているkwは探索しない
+                print(f'探索KW：{kw}')
 
-            limit_cnt += 1
-            if limit_cnt >= 10:
-                return search_result
+                url = kw_url_dict[kw]
+                r = get_web_page(url)
+                kw_list_tmp, kw_url_dict_tmp = kw_scraping(r)
+                kw_list_tmp = judgement_no_target_search(kw_list_tmp, search_result)
+                
+                print(f'取得したKWのlist：{kw_list_tmp}')
+                search_result[kw] = kw_list_tmp
+                next_search_kw_list += kw_list_tmp
+                next_search_url_dict = kw_url_dict_tmp | next_search_url_dict
 
-            time.sleep(1)
+                limit_cnt += 1
+                if limit_cnt >= search_limit_times:
+                    return search_result
 
-        else:
-            pass
+            else:
+                pass
+    
+    else:
+        print('探索対象が存在しません') # kw_listが空の場合は処理を抜ける
+        return search_result
 
     return web_search(next_search_kw_list, next_search_url_dict, search_result, limit_cnt)
 
 def search_result_conversion(search_result):
+    """
+    探索を未実施のKWに$を付与する
+
+    Parameters
+    ----------
+    search_result : dict
+        探索元のKWと取得したKWのdict {'物流': ['英語$', 'ロジスティクス', ...], 'ロジスティクス':[...], ...}
+
+    Returns
+    -------
+    search_result : dict
+        探索していないKWに$を付与したdict
+
+    """
 
     search_result_tmp = {}
-    no_conversion_list = ['@', '$']
 
     for key in search_result.keys():
         result_list_tmp = []
 
         for result in search_result[key]:
-            if result not in search_result.keys() and result[-1] not in no_conversion_list:
+            if result not in search_result.keys() and result[-1] not in search_unexecuted_flag:
                 result_list_tmp.append(result+'$')
             else:
                 result_list_tmp.append(result)
@@ -145,17 +185,34 @@ def search_result_conversion(search_result):
 
     return search_result_tmp
 
-def tree_display(search_result, parent_kw, cnt):
-    print('  '*cnt+'-'+parent_kw)
-    cnt = cnt + 1
+def search_result_output(search_result, parent_kw, depth_cnt):
+    """
+    探索結果をN分木で再帰的に出力する
+
+    Parameters
+    ----------
+    search_result : dict
+        探索元のKWと取得したKWのdict {'物流': ['英語$', 'ロジスティクス', ...], 'ロジスティクス':[...], ...}
+
+    parent_kw : str
+        探索元のKW
+
+    depth_cnt : int
+        探索の深さのカウント
+
+    """
+
+    print('    '*depth_cnt+'-'+parent_kw)
+    depth_cnt = depth_cnt + 1
 
     for i, children_kw in enumerate(search_result[parent_kw]):
-        if children_kw[-1] not in ['@', '$']:
-            tree_display(search_result, children_kw, cnt)
-        elif i != len(search_result[parent_kw])-1:
-            print('  '*cnt+'-'+children_kw)
+
+        if children_kw[-1] not in search_unexecuted_flag: # 末尾に$or@が付与されていないkwは探索結果を表示させる
+            search_result_output(search_result, children_kw, depth_cnt)
+        elif i != len(search_result[parent_kw])-1: # $or@が付与されていて表示してない探索kwが残っている場合はkwを表示するだけ
+            print('    '*depth_cnt+'-'+children_kw)
         else:
-            print('  '*cnt+'-'+children_kw)
+            print('    '*depth_cnt+'-'+children_kw) # $or@が付与されていて表示してない探索kwが残っていない場合はkwを表示した後にreturnする
             return
 
 
@@ -165,14 +222,23 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--target_kw', type=str, default=None, help='ターゲットKW')
     args = parser.parse_args()
 
+    search_limit_times = 20 # 探索上限回数
+    no_target_search_flag = ['語', '学'] # 探索しないKWの条件
+    search_unexecuted_flag = ['@', '$'] # 探索未実施のkwに付与する値
+
     if args.target_kw is not None:
 
         kw_list = [args.target_kw]
         url_dict = {args.target_kw:'https://ja.wikipedia.org/wiki/'+urllib.parse.quote(args.target_kw)}
         search_result = web_search(kw_list, url_dict, search_result={}, limit_cnt=0)
+
+        if len(search_result.keys()) == 1:
+            print('探索対象が0件でした')
+            sys.exit()
+
         search_result = search_result_conversion(search_result)
 
-        tree_display(search_result, parent_kw=args.target_kw, cnt=0)
+        search_result_output(search_result, parent_kw=args.target_kw, depth_cnt=0)
 
         out_put_path = '../data/'
         os.makedirs(os.path.dirname(out_put_path), exist_ok=True)
@@ -181,4 +247,3 @@ if __name__ == '__main__':
 
     else:
         print('kw is None')
-
